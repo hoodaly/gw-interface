ab283d3790a6635dc95552e139238dabfdf15db0
hoodaly <hoodaly@gmail.com>
2019-09-22 10:39:38 +0200

    Implement the Log functions

--- GuildWarsInterface/Debugging/Debug.cs
+++ GuildWarsInterface/Debugging/Debug.cs
@@ -18,5 +18,19 @@ namespace GuildWarsInterface.Debugging
                                 ThrowException(new Exception("precondition violated"));
                         }
                 }
+
+                public static void Log(string v)
+                {
+                        System.Diagnostics.Debug.Write(v + "\n");
+                }
+
+                public static void LogBytes(byte[] data, string prefix)
+                {
+                        if (data.Length <= 0)
+                        {
+                                return;
+                        }
+                        System.Diagnostics.Debug.Write(prefix + " " + BitConverter.ToString(data) + "\n");
+                }
         }
 }
