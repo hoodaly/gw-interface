695bad882f91776d323b0e5f4adcde0229f8b36d
hoodaly <hoodaly@gmail.com>
2019-09-22 10:50:28 +0200

    New DisableEncryption, plus oldschool memory searching hooking helper

--- GuildWarsInterface/Modification/Hooks/HookHelper.cs
+++ GuildWarsInterface/Modification/Hooks/HookHelper.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Binarysharp.Assemblers.Fasm;
 using GuildWarsInterface.Modification.Native;
@@ -24,6 +25,41 @@ namespace GuildWarsInterface.Modification.Hooks
                         Kernel32.VirtualProtect(from, 5, 0x40, out dwOldProtection);
                         Marshal.Copy(hook, 0, from, 5);
                         Kernel32.VirtualProtect(from, 5, dwOldProtection, out dwOldProtection);
+                }
+
+                
+                public static List<int> searchAsm(byte[] target)
+                {
+                        List<int> output = new List<int>();
+                        int cur = 0x00401000;
+                        int end = 0x00800000;
+                        int idx = 0;
+                        int chunkstart = cur;
+                        while (cur < end)
+                        {
+                                if (target[idx] == Marshal.ReadByte((IntPtr)cur))
+                                {
+                                        if (idx == 0)
+                                        {
+                                                chunkstart = cur;
+                                        }
+                                        idx += 1;
+                                        if (idx == target.Length)
+                                        {
+                                                // found it
+                                                output.Add(chunkstart);
+                                                idx = 0;
+                                        }
+                                        cur += 1;
+                                }
+                                else
+                                {
+                                        cur = chunkstart + 1;
+                                        chunkstart = cur;
+                                        idx = 0;
+                                }
+                        }
+                        return output;
                 }
         }
 }
--- GuildWarsInterface/Modification/Patches/DisableEncryptionPatch.cs
+++ GuildWarsInterface/Modification/Patches/DisableEncryptionPatch.cs
@@ -1,7 +1,9 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using GuildWarsInterface.Modification.Hooks;
 using GuildWarsInterface.Modification.Native;
 
 #endregion
@@ -10,14 +12,17 @@ namespace GuildWarsInterface.Modification.Patches
 {
         internal static class DisableEncryptionPatch
         {
-                private static readonly IntPtr _location = (IntPtr) 0x005900BB;
-
                 public static void Apply()
                 {
-                        uint dwOldProtection;
-                        Kernel32.VirtualProtect(_location, 5, 0x40, out dwOldProtection);
-                        Marshal.Copy(new byte[] {0x8A, 0x14, 0x18}, 0, _location, 3);
-                        Kernel32.VirtualProtect(_location, 5, dwOldProtection, out dwOldProtection);
+                        List<int> addrs = HookHelper.searchAsm(new byte[] { 0x89, 0x55, 0x10, 0x8B, 0x55, 0x08, 0x89, 0x55, 0x08, 0x46, 0x81, 0xE6, 0xFF, 0x00, 0x00, 0x00 });
+                        foreach(int addr in addrs)
+                        {
+                                IntPtr location = (IntPtr)(addr + 0x41);
+                                uint dwOldProtection;
+                                Kernel32.VirtualProtect(location, 5, 0x40, out dwOldProtection);
+                                Marshal.Copy(new byte[] {0x8A, 0x14, 0x18}, 0, location, 3);
+                                Kernel32.VirtualProtect(location, 5, dwOldProtection, out dwOldProtection);
+                        }
                 }
         }
 }
