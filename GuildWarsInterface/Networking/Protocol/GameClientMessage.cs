7c56397b6ae89b4e01a6ff50dc660bc1e25335db
hoodaly <hoodaly@gmail.com>
2020-07-01 00:36:33 +0200

    Update Network Packet  IDs
    Copied from GWCA project :)

--- GuildWarsInterface/Networking/Protocol/GameClientMessage.cs
+++ GuildWarsInterface/Networking/Protocol/GameClientMessage.cs
@@ -1,40 +1,40 @@
-namespace GuildWarsInterface.Networking.Protocol
+﻿namespace GuildWarsInterface.Networking.Protocol
 {
         internal enum GameClientMessage
         {
                 ExitToCharacterScreen = 2,
                 ExitToLoginScreen = 3,
-                CommitMapChange = 9,//na,
-                EquipItem = 54,//42,
-                ChangeWeaponset = 56,//44,
-                Attack = 57,//45,
-                KeyboardMove = 67,//55,
-                MouseMove = 68,//56,
-                CastSkill = 76,//64,
-                BuyItem = 83,//71,
-                UnEquipItem = 85,//73,
-                KeyboardStopMoving = 77,
-                StylistChangeAppearance = 78,
-                EquipSkill = 98,//85,
-                SwapSkill = 100,//87,
-                MoveSkillToEmptySlot = 101,//88,
-                CharacterCreateUpdateCampaignAndProfession = 102,//89,
-                Chat = 106,//94,
-                MoveBag = 110,
-                UnEquipBag = 119,
-                MoveItem = 120,//108,
-                CreateNewCharacter = 143,//131,
-                ValidateNewCharacter = 145,//133,
-                InstanceLoadRequestMapData = 142,
-                InstanceLoadRequestPlayerData = 151,
-                InstanceLoadRequestItems = 152,
-                EnterFight = 172,
-                AcceptJoin = 163,//150, //controllerManager.RegisterHandler(150, AcceptJoinRequestHandler);
-                KickInvite = 164,//151, //controllerManager.RegisterHandler(151, KickInviteHandler);
-                KickJoinRequest = 165,//152, //controllerManager.RegisterHandler(152, KickJoinRequestHandler);
-                Invite = 167,//154, //controllerManager.RegisterHandler(154, InviteHandler);
-                Leave = 169,//156, //controllerManager.RegisterHandler(156, LeaveHandler);
-                KickMember = 176,//163, //controllerManager.RegisterHandler(163, KickMemberHandler);*/
-                ChangeMap = 184,//171,
+                CommitMapChange = 9,//na, //Disconnect
+                EquipItem = 55,//42,
+                ChangeWeaponset = 57,//44,
+                Attack = 58,//45,
+                KeyboardMove = 68,//55,
+                MouseMove = 69,//56,
+                CastSkill = 77,//64,
+                BuyItem = 84,//71,
+                UnEquipItem = 86,//73,
+                KeyboardStopMoving = 78,
+                StylistChangeAppearance = 79,
+                EquipSkill = 99,//85,
+                SwapSkill = 101,//87,
+                MoveSkillToEmptySlot = 102,//88,
+                CharacterCreateUpdateCampaignAndProfession = 103,//89,
+                Chat = 107,//94,
+                MoveBag = 111,
+                UnEquipBag = 120,
+                MoveItem = 121,//108,
+                CreateNewCharacter = 144,//131,
+                ValidateNewCharacter = 146,//133,
+                InstanceLoadRequestMapData = 143,
+                InstanceLoadRequestPlayerData = 152,
+                InstanceLoadRequestItems = 153,
+                EnterFight = 173,
+                AcceptJoin = 164,//150, //controllerManager.RegisterHandler(150, AcceptJoinRequestHandler);
+                KickInvite = 165,//151, //controllerManager.RegisterHandler(151, KickInviteHandler);
+                KickJoinRequest = 166,//152, //controllerManager.RegisterHandler(152, KickJoinRequestHandler);
+                Invite = 168,//154, //controllerManager.RegisterHandler(154, InviteHandler);
+                Leave = 170,//156, //controllerManager.RegisterHandler(156, LeaveHandler);
+                KickMember = 177,//163, //controllerManager.RegisterHandler(163, KickMemberHandler);*/
+                ChangeMap = 185,//171,
         }
 }
